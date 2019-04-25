f0c39e8a4036c1388722bb7968c39fa3c6b29e1f
Stefanos Klironomos <kliron@tuta.io>
2019-04-25 12:34:47 +0200

    Added pre- and postprocessing

--- tokenization.py
+++ tokenization.py
@@ -40,6 +40,71 @@ TK_MAJ, TK_UP, TK_REP, TK_WREP = 'xxmaj', 'xxup', 'xxrep', 'xxwrep'
 
 text_spec_tok = [UNK, PAD, BOS, EOS, FLD, TK_MAJ, TK_UP, TK_REP, TK_WREP]
 
+def spec_add_spaces(t: str) -> str:
+    """Add spaces around / and # in `t`. \n"""
+    return re.sub(r'([/#\n])', r' \1 ', t)
+
+
+def rm_useless_spaces(t: str) -> str:
+    """Remove multiple spaces in `t`."""
+    return re.sub(' {2,}', ' ', t)
+
+
+def replace_rep(t: str) -> str:
+    """Replace repetitions at the character level in `t`."""
+    def _replace_rep(m: Collection[str]) -> str:
+        c, cc = m.groups()
+        return f' {TK_REP} {len(cc)+1} {c} '
+    re_rep = re.compile(r'(\S)(\1{3,})')
+    return re_rep.sub(_replace_rep, t)
+
+
+def replace_wrep(t: str) -> str:
+    """Replace word repetitions in `t`."""
+    def _replace_wrep(m: Collection[str]) -> str:
+        c, cc = m.groups()
+        return f' {TK_WREP} {len(cc.split())+1} {c} '
+    re_wrep = re.compile(r'(\b\w+\W+)(\1{3,})')
+    return re_wrep.sub(_replace_wrep, t)
+
+
+def fix_html(x: str) -> str:
+    """List of replacements from html strings in `x`."""
+    re1 = re.compile(r'  +')
+    x = x.replace('#39;', "'").replace('amp;', '&').replace('#146;', "'").replace(
+        'nbsp;', ' ').replace('#36;', '$').replace('\\n', "\n").replace('quot;', "'").replace(
+        '<br />', "\n").replace('\\"', '"').replace('<unk>', UNK).replace(' @.@ ', '.').replace(
+        ' @-@ ', '-').replace(' @,@ ', ',').replace('\\', ' \\ ')
+    return re1.sub(' ', html.unescape(x))
+
+
+def replace_all_caps(x: Collection[str]) -> Collection[str]:
+    """Replace tokens in ALL CAPS in `x` by their lower version and add `TK_UP` before."""
+    res = []
+    for t in x:
+        if t.isupper() and len(t) > 1:
+            res.append(TK_UP)
+            res.append(t.lower())
+        else:
+            res.append(t)
+    return res
+
+
+def deal_caps(x: Collection[str]) -> Collection[str]:
+    """Replace all Capitalized tokens in `x` by their lower version and add `TK_MAJ` before."""
+    res = []
+    for t in x:
+        if t == '':
+            continue
+        if t[0].isupper() and len(t) > 1 and t[1:].islower():
+            res.append(TK_MAJ)
+        res.append(t.lower())
+    return res
+
+
+# Preprocess and postprocess rules for tokenization
+pre_rules = [fix_html, replace_rep, replace_wrep, spec_add_spaces, rm_useless_spaces]
+post_rules = [replace_all_caps, deal_caps]
 
 def batch_filenames(batch_size: int) -> Collection[Collection[str]]:
     """Returns batch_size number of lists of pathnames to textfiles for processing."""
@@ -52,10 +117,14 @@ def batch_filenames(batch_size: int) -> Collection[Collection[str]]:
 
 
 def text_gen(path_list: Collection[str]) -> Generator[str, None, None]:
-    """Reads files from path_list and returns their text content sequentially."""
+    """Reads files from path_list and returns their text content sequentially
+    after applying pre-processing rules."""
     for path in path_list:
         with open(path, 'r') as r:
-            yield r.read()
+            txt = r.read()
+            for fn in pre_rules:
+                txt = fn(txt)
+            yield txt                
 
 
 def process_files(lang: str,
@@ -82,8 +151,10 @@ def process_files(lang: str,
         for docs in nlp.pipe(texts, batch_size=batch_size):
             gc.collect()
             tokens = [t.text for t in docs]
-            counts += Counter(tokens)
+            for fn in post_rules:
+                tokens = fn(tokens)            
             w.write(' '.join(tokens))
+            counts += Counter(tokens)
 
     queue.put(counts)
     print('Process {} finished.'.format(pid))
