e796c64e71c3dea5750ea3b48016e7fe8d01cd48
Stefanos Klironomos <kliron@tuta.io>
2019-04-26 09:40:14 +0200

    Cleaned up a bit and added progress reporting

--- tokenization.py
+++ tokenization.py
@@ -1,7 +1,7 @@
 #!/usr/bin env python3
 
 import os
-from typing import Collection, Generator
+from typing import Collection, List, Generator, Callable
 import spacy
 from spacy.attrs import ORTH
 from collections import Counter
@@ -10,35 +10,28 @@ import numpy as np
 import re
 import html
 import gc
-import pathlib
+from pathlib import Path
+import pandas as pd
+import csv
+from fastai.text import Vocab
+import tqdm
 
-txt_dir = os.path.join(pathlib.Path.home(), 'Data/corpora')
-subdirs = [
-    'fass',
-    'lakartidningen',
-    'radiology',
-    'literaturbanken',
-    'svwiki',
-    'riksdagen',
-    'riksdagen1',
-    'riksdagen2',
-    'riksdagen3',
-    'riksdagen4',
-    'riksdagen5',
-    'riksdagen6',
-    'riksdagen7',
-    'riksdagen8',
-    'riksdagen9'
-]
-tokens_out_file_format = os.path.join(txt_dir, 'tokens{}.txt')
-# just a list of words sorted from most to least common
-vocab_out_file = os.path.join(txt_dir, 'vocab.txt')
+root_dir = Path(Path.home(), 'Data/corpora')
+txt_dir = Path(root_dir, 'texts')
+tokens_dir = Path(root_dir, 'tokens')
+tokens_out_file_format = os.path.join(tokens_dir, 'tokens{}_temp')
+# Flat text file containing _all_ tokens in descending order of frequency
+words_file = Path(tokens_dir, 'words.pkl')
+# Serialized Vocab
+vocab_file = Path(tokens_dir, 'vocab.pkl')
+tokens_file = Path(tokens_dir, 'tokens.txt')
 
 # Special tokens
 BOS, EOS, FLD, UNK, PAD = 'xxbos', 'xxeos', 'xxfld', 'xxunk', 'xxpad'
 TK_MAJ, TK_UP, TK_REP, TK_WREP = 'xxmaj', 'xxup', 'xxrep', 'xxwrep'
 
 text_spec_tok = [UNK, PAD, BOS, EOS, FLD, TK_MAJ, TK_UP, TK_REP, TK_WREP]
+
 
 def spec_add_spaces(t: str) -> str:
     """Add spaces around / and # in `t`. \n"""
@@ -106,12 +99,14 @@ def deal_caps(x: Collection[str]) -> Collection[str]:
 pre_rules = [fix_html, replace_rep, replace_wrep, spec_add_spaces, rm_useless_spaces]
 post_rules = [replace_all_caps, deal_caps]
 
-def batch_filenames(batch_size: int) -> Collection[Collection[str]]:
-    """Returns batch_size number of lists of pathnames to textfiles for processing."""
+
+def batch_filenames(batch_size: int) -> List[List[str]]:
+    """Recursively reads all files under `txt_dir` and returns batch_size 
+    number of lists of pathnames for processing."""
     file_paths = []
-    for d in subdirs:
-        dir_path = os.path.join(txt_dir, d)
-        file_paths += [os.path.join(dir_path, f) for f in os.listdir(dir_path) if f.endswith('.txt')]
+    for root, dirs, files in os.walk(txt_dir):
+        for f in files:
+            file_paths.append(os.path.join(root, f))
     np.random.shuffle(file_paths)
     return np.array_split(file_paths, batch_size)
 
@@ -128,9 +123,10 @@ def text_gen(path_list: Collection[str]) -> Generator[str, None, None]:
 
 
 def process_files(lang: str,
-                  path_list: Collection[str],
-                  queue: Queue,
+                  path_list: Collection[str],                  
                   output_file: str,
+                  data_queue: Queue,
+                  progress_queue: Queue,
                   batch_size: int = 5000):
     """Extracts tokens from each file in path_list and keeps a Counter of all tokens.
     Writes tokens as space separated strings in `output_file`.
@@ -155,43 +151,85 @@ def process_files(lang: str,
                 tokens = fn(tokens)            
             w.write(' '.join(tokens))
             counts += Counter(tokens)
+            progress_queue.put(1)
 
-    queue.put(counts)
-    print('Process {} finished.'.format(pid))
+    data_queue.put(counts)
 
 
-def main(lang: str, n_workers: int, max_vocab: int = 60000):
+def postprocess(file: str):
+    """Some rows are NaN when read in pandas, remove them and save the corpus as plaintext"""
+    df = pd.read_csv(file, header=None, sep='\b')
+    df = df.dropna()    
+    df.to_csv(file,
+              index=False,
+              header=None,
+              sep='\b',
+              line_terminator='\n',
+              quoting=csv.QUOTE_NONE,
+              escapechar='\\')
+
+
+def tokenize(lang: str, n_workers: int, tfile: str, postprocess_fn: Callable = postprocess):
     """
-    Creates a shared queue and forks `n_workers` tokenizer processes. Waits for all processes to
-    finish and then merges all worker counters.
-    WARNING! Don't put very large objects on multiprocessing.Queue. Queue.get is extremely slow. The
-    performance of reading through IPC in python is atrocious.
-    """
-    queue = Queue(maxsize=n_workers)
-    c = Counter()
+    1. Creates a shared queue and forks `n_workers` tokenizer processes. Waits for all processes to
+    finish and then merges all worker counters. 
+    2. Concatenates all token files into a single file `tfile` and removes the component files.
+    3. Calls `postprocess` fn on the resulting file path.
+    The resulting file should be a plaintext file where each line consists of space-separated tokens from a single processed 
+    initial text file.
+    """    
     batches = batch_filenames(batch_size=n_workers)
+    output_files = []
     processes = []
+    data_queue = Queue(maxsize=n_workers)
+    progress_queue = Queue()
+    counter = Counter()
 
     for i, batch in enumerate(batches):
-        batch = batch
+        output_file = tokens_out_file_format.format(i+1)
+        output_files.append(output_file)
         processes.append(Process(target=process_files,
-                                 args=(lang, batch, queue, tokens_out_file_format.format(i+1))))
+                                 args=(lang, batch, output_file, data_queue, progress_queue)))
     for p in processes:
         p.start()
 
+    # Report progress
+    total = 0
+    for b in batches:
+        total += len(b)
+    pbar = tqdm.tqdm(total=total)
+    i = 0
+    while i < total:
+        _ = progress_queue.get()
+        pbar.update()
+        i += 1
+
     for _ in processes:
-        counter = queue.get()
-        c += counter
+        c = data_queue.get()
+        counter += c
 
     for p in processes:
         p.join()
 
-    with open(vocab_out_file, 'w') as w:
-        print('Tokenizing done. Writing vocabulary.')
-        w.write(' '.join([items[0] for items in c.most_common(max_vocab)]))
+    with open(vocab_file, 'w') as w:
+        print('Tokenizing done. Writing vocabulary...')
+        w.write(' '.join([items[0] for items in counter.most_common()]))
 
-    print('All done.')
+    with open(tfile, 'w') as w:
+        for f in output_files:
+            with open(f, 'r') as r:
+                w.write(r.read())
+            os.remove(f)
+
+    postprocess_fn(tfile)
+
+
+def main():
+    print('Tokens file is going to be saved at {}'.format(tokens_file))
+    tokenize(lang='sv',
+             n_workers=cpu_count()-1,
+             tfile=tokens_file)
 
 
 if __name__ == '__main__':
-    main(lang='sv', n_workers=cpu_count()-1)
+    main()
